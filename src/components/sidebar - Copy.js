04217d9ed19087edbbdea9dc0df872dc17b90c46
arsal231 <arslan.dev231@gmail.com>
2023-07-16 20:42:25 +0500

    new files added

---
+++ src/components/sidebar - Copy.js
@@ -0,0 +1,21 @@
+
+const Sidebar = ()=>{
+
+    return (
+     <>
+        <p>SideBar</p>
+        <ul>
+            <li> Category1</li>
+            <li> Category2</li>
+            <li> Category3</li>
+            <li> Category4</li>
+               
+            
+        </ul>
+        
+     </>
+    )
+
+}
+
+export default Sidebar;
